f14c1d1f21cb69daa60b8dc334dae66cebf80342
Halil <svtr.hozcan@gmail.com>
2026-04-27 15:58:15 +0200

    fourthsecond

--- build.sh
+++ build.sh
@@ -2,3 +2,12 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'back@foreningsnodroppen.se', 'Bylsfrlmz42.')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
