fd57d858539ae5de167623ff68e62d0a00a9f353
Halil <svtr.hozcan@gmail.com>
2026-05-02 17:09:50 +0200

    environment4

--- main/settings.py
+++ main/settings.py
@@ -215,7 +215,7 @@ CORS_ALLOWED_ORIGINS = [
     #'http://127.0.0.1:8000',
     'https://foreningsnodroppen.se',
     'https://www.foreningsnodroppen.se',
-    'https://skfnx.vercel.app/',
+    'https://skfnx.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
